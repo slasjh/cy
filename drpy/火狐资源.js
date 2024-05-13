407d2e323f7076f6101595d4c438c5f5b497d3fb
slasjh <19239471-slasjh@users.noreply.gitlab.com>
2024-05-13 00:30:58 +0000

    Add new file

---
+++ drpy/火狐资源.js
@@ -0,0 +1,11 @@
+// https://hhzyapi.com/api.php/provide/vod/?ac=list
+
+var rule = {
+    模板: '采集1',
+    title: '火狐资源',
+    host: 'https://hhzyapi.com',
+    // homeTid: '13',
+    homeTid: '',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯|伦理片',
+    parse_url: '',
+}
