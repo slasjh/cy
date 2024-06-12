668688d0ebb060c34116810397d96ae4f0455ef6
slasjh <19239471-slasjh@users.noreply.gitlab.com>
2024-06-12 07:09:16 +0000

    Update 两个BT.js

--- drpy/两个BT.js
+++ drpy/两个BT.js
@@ -7,7 +7,7 @@ var rule = {
     searchable:0,
     quickSearch:0,
     headers:{
-        'User-Agent':'UC_UA'
+        'User-Agent':'MOBILE_UA#Accept$text/html'
     },
     timeout:5000,
     class_name:'最新电影&国产剧&美剧&日韩剧',//静态分类名称拼接
