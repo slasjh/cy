67c96129a8feb77f605dff5b54b2f799603ebf6e
slasjh <19239471-slasjh@users.noreply.gitlab.com>
2024-06-12 07:28:48 +0000

    Update 两个BT.js

--- drpy/两个BT.js
+++ drpy/两个BT.js
@@ -9,6 +9,7 @@ var rule = {
     headers:{
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
         'method':'GET',
+        'Referer':'https://www.bttwoo.com/'
 
     },//        'Referer':'/xssearch?q=**&f=_all&p=1'
     timeout:5000,
