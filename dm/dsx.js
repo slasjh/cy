b108b40fc0a27907078d873284f377b5ddecd038
slasjh <19239471-slasjh@users.noreply.gitlab.com>
2024-07-26 07:04:29 +0000

    Update dsx.js

--- dm/dsx.js
+++ dm/dsx.js
@@ -8,7 +8,7 @@ var rule = {
 	// url:'/vodshow/fyclass--------fypage---.html',
 	url: '/vodshow/fyfilter.html',
 	filterable: 1,//是否启用分类筛选,
-	filter_url: '{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
+	filter_url: '4-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
 	class_name:'动漫',
     class_url:'4',
 
